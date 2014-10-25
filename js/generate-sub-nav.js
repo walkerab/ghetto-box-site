e34ccdd24d454b415e1c611c50e8df4e4643cf7c
Andrew Walker <woxorz@gmail.com>
2014-10-25 15:54:26 -0700

    sub nav generator

---
+++ js/generate-sub-nav.js
@@ -0,0 +1,19 @@
+var elheadings = document.getElementsByTagName('h2');
+var len = elheadings.length;
+var elsub_navs_content = [];
+for (var i = 0; i < len; i++) {
+	var elheading = elheadings[i];
+	elheading.id = elheading.textContent.toLowerCase().replace(' ','-');
+	elsub_navs_content.push(
+		'<li class="stack pad-h-lg pad-v-sm cs-',
+			(i%2)+1, // alternate 1 and 2
+			'"><a href="#',elheading.id,'">',
+			elheading.textContent,
+		'</a></li>');
+}
+var elsub_navs = document.getElementsByClassName('js-sub-nav');
+len = elsub_navs.length;
+elsub_navs_content = elsub_navs_content.join('');
+for (var i = 0; i < len; i++) {
+	elsub_navs[i].innerHTML = elsub_navs_content;
+}

038e9d0fe05194c3309c0cf5a9c3d60a6927ef15
Andrew Walker <woxorz@gmail.com>
2014-12-16 19:55:59 -0800

    Made sub-nav look decent on mobile

--- js/generate-sub-nav.js
+++ js/generate-sub-nav.js
@@ -6,12 +6,22 @@
 		var elheading = elheadings[i];
 		elheading.id = elheading.textContent.toLowerCase().replace(/\W/g,'-');
 		elsub_nav_content.push([
-			'<li class="tile w-1-3 md-w-fill md-stack cs-',
+			'<li class="table-cell w-1-3 md-w-fill md-stack cs-',
 				(i%2)+1, // alternate 1 and 2
 				'"><a class="stack pad-h-md lg-pad-h-lg pad-v-sm" href="#',elheading.id,'">',
 				elheading.textContent,
 			'</a></li>'].join(''));
 	}
+	var rows = [];
+	for (var i = 0; i < elsub_nav_content.length; i+=3) {
+		rows.push('<div class="table-row md-stack">' + elsub_nav_content.slice(i,i+3).join('') + '</div>');
+	}
 	var elsub_nav = document.getElementsByClassName('js-sub-nav')[0];
-	elsub_nav.innerHTML = elsub_nav_content.join('\n');
+	elsub_nav.innerHTML = rows.join('\n');
+
+	if (rows.length % 2 === 0) {
+		elsub_nav.classList.add('cs-2');
+	} else {
+		elsub_nav.classList.add('cs-1');
+	}
 })();

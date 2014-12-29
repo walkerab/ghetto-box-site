f68f747c616576fbfaf5d9446b884b830686fa57
Andrew Walker <woxorz@gmail.com>
2014-12-29 13:12:10 -0800

    sub-nav no longer has background colour assigned to it and instead just generates filler cells

--- js/generate-sub-nav.js
+++ js/generate-sub-nav.js
@@ -2,7 +2,8 @@
 	var elheadings = document.getElementsByTagName('h2');
 	var len = elheadings.length;
 	var elsub_nav_content = [];
-	for (var i = 0; i < len; i++) {
+	var i = 0;
+	for (; i < len; i++) {
 		var elheading = elheadings[i];
 		elheading.id = elheading.textContent.toLowerCase().replace(/\W/g,'-');
 		elsub_nav_content.push([
@@ -12,16 +13,16 @@
 				elheading.textContent,
 			'</a></li>'].join(''));
 	}
+	for(; i % 3 !== 0; i++) {
+		elsub_nav_content.push([
+			'<li class="table-cell table-cell-middle w-1-3 md-hide cs-',
+				(i%2)+1, // alternate 1 and 2
+				'">&nbsp;</li>'].join(''));
+	}
 	var rows = [];
 	for (var i = 0; i < elsub_nav_content.length; i+=3) {
 		rows.push('<div class="table-row md-stack">' + elsub_nav_content.slice(i,i+3).join('') + '</div>');
 	}
 	var elsub_nav = document.getElementsByClassName('js-sub-nav')[0];
 	elsub_nav.innerHTML = rows.join('\n');
-
-	if (rows.length % 2 === 0) {
-		elsub_nav.classList.add('cs-2');
-	} else {
-		elsub_nav.classList.add('cs-1');
-	}
 })();
